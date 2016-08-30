0dd6d48bf199d047aba710981bd5ee4c083aad2a
stevevice <steve_alexanders@bellsouth.net>
2016-08-30 17:52:05 -0500

    creating spheres

--- OpenGL/Source.cpp
+++ OpenGL/Source.cpp
@@ -14,7 +14,6 @@ int main()
 	if (glfwInit() == false)
 		return -1;
 
-
 	GLFWwindow* window = glfwCreateWindow(1280, 720, "Graphics Window", nullptr, nullptr);
 
 	if (window == nullptr)
@@ -24,8 +23,6 @@ int main()
 	}
 
 	glfwMakeContextCurrent(window);
-
-	// Code Starts here
 
 	if (ogl_LoadFunctions() == ogl_LOAD_FAILED) {
 		glfwDestroyWindow(window);
@@ -37,6 +34,14 @@ int main()
 	auto minor = ogl_GetMinorVersion();
 	printf("GL: %i.%i\n", major, minor);
 
+	Gizmos::create();
+	mat4 view = glm::lookAt(vec3(10, 10, 10), vec3(0), vec3(0, 1, 0));
+	mat4 projection = glm::perspective(glm::pi<float>() * .25f, 16 / 9.f, .1f /*doesnt change anything but if number is too large then code breaks*/
+		, 1000.f /*doesnt change anything*/);
+
+	glClearColor(0.25f, 0.25f, 0.25f, 1);
+	glEnable(GL_DEPTH_TEST); // enables the depth buffer
+
 	while (glfwWindowShouldClose(window) == false &&
 		glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS)
 	{
@@ -45,41 +50,36 @@ int main()
 		Gizmos::clear();
 
 		Gizmos::addTransform(glm::mat4(1));
+		
+		//Code starts here
+		vec4 yellow = vec4(255, 255, 0, .5); //Red, green, blue, transparency
+		vec4 blue = vec4(0, 0, 255, .8);
+		//mat4 sunrot = glm::rotate( 0, glm::vec3(0, 1, 0));	//Rotate Sun
+		mat4 Sun = mat4(1); //* sunrot;
+		Gizmos::addSphere(vec3(Sun[0]), 1, 50, 50, yellow, &Sun);
+		Gizmos::addSphere(vec3(Sun[1]), 1, 50, 50, blue, &Sun);
 
+		//code end here
 		vec4 white(1);
 		vec4 black(0, 0, 0, 1);
 
-		for (int i = 0; i < 21; ++i) 
+		for (int i = 0; i < 21; ++i)
 		{
-			Gizmos::addLine(vec3(-10 + i, 0, 10), vec3(-10 + i, 0, -10), i == 10 ? white : black);
-			Gizmos::addLine(vec3(10, 0, -10 + i), vec3(-10, 0, -10 + i), i == 10 ? white : black);
+			Gizmos::addLine(vec3(-10 + i, 0, 10),
+				vec3(-10 + i, 0, -10),
+				i == 10 ? white : black);
+
+			Gizmos::addLine(vec3(10, 0, -10 + i),
+				vec3(-10, 0, -10 + i),
+				i == 10 ? white : black);
 		}
-
 		Gizmos::draw(projection * view);
-
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
-	glClearColor(0.25f, 0.25f, 0.25f, 1);
-
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	glClearColor(0.25f, 0.25f, 0.25f, 1);
-	glEnable(GL_DEPTH_TEST); // enables the depth buffer
-
-	Gizmos::create();
-
-	mat4 view = glm::lookAt(vec3(10, 10, 10), vec3(0), vec3(0, 1, 0));
-	mat4 projection = glm::perspective(glm::pi<float>() * .25f, 16.9f, 0.1f, 1000.f);
-
 	Gizmos::destroy();
-
-
-	// Code Ends here
-
-
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
