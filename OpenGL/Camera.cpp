a7318833b9d2496d2ddd532bf1c79015e747d960
stevevice <steve_alexanders@bellsouth.net>
2016-09-20 18:16:51 -0500

    about to start on camera

---
+++ OpenGL/Camera.cpp
@@ -0,0 +1,11 @@
+#include "Camera.h"
+#include <glfw3.h>
+#include <glm/ext.hpp>
+
+Camera::Camera()
+{
+}
+
+Camera::~Camera()
+{
+}
---
+++ OpenGL/Camera.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <glm/vec3.hpp>
+#include <glm/mat4x4.hpp>
+
+class  Camera
+{
+public:
+	 Camera();
+	~ Camera();
+
+private:
+
+};
